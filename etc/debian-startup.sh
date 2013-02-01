3344778bf83981cda78d130b591a8299d667fb79
Janne Snabb <snabb@epipe.com>
2013-02-01 21:17:12 +0200

    Do not depend on "zfs" in Debian init script as the zfs init script is not enabled by default. The ZFS startup is actually done by udev, thus depend on "udev".

--- etc/debian-startup.sh
+++ etc/debian-startup.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 ### BEGIN INIT INFO
 # Provides:          zfswatcher
-# Required-Start:    zfs $syslog $network $remote_fs
-# Required-Stop:     zfs $syslog $network $remote_fs
+# Required-Start:    udev $syslog $network $remote_fs
+# Required-Stop:     udev $syslog $network $remote_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: ZFS monitoring daemon
