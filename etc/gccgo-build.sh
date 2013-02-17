f6d97cb5fe5c25f168eb1de2479bdfb1de05f276
Janne Snabb <snabb@epipe.com>
2013-02-17 04:44:57 +0200

    Move ZFS parsing to a separate file.

--- etc/gccgo-build.sh
+++ etc/gccgo-build.sh
@@ -47,7 +47,7 @@ compile_go_package zfswatcher.damicon.fi/notifier \
 set -x
 $GCCGO -I $INC -o zfswatcher \
 	zfswatcher.go leds.go util.go version.go webserver.go \
-	setup.go \
+	setup.go zparse.go \
 	osutil_$GOOS.go \
 	$subpackages
 
